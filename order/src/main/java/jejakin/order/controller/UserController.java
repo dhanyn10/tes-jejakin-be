fcac33243d82153341ea15d225aeb45e8c2e69c7
dhanyn10 <dhanyn.dhan@gmail.com>
2023-01-16 19:41:47 +0700

    login

--- order/src/main/java/jejakin/order/controller/UserController.java
+++ order/src/main/java/jejakin/order/controller/UserController.java
@@ -2,10 +2,14 @@ package jejakin.order.controller;
 
 import java.util.List;
 
+import javax.servlet.http.HttpSession;
+
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import jejakin.order.dao.UserRepository;
@@ -17,14 +21,27 @@ public class UserController {
 	@Autowired
 	private UserRepository userRepo;
 	
-	@PostMapping("/adduser")
+	@PostMapping("adduser")
 	public String saveUser (@RequestBody User user){
 		userRepo.save(user);
 		return "user ditambahkan dengan" + user.getId();
 	}
 	
-	@GetMapping("/users")
+	@GetMapping("users")
 	public List<User> getUser() {
 		return userRepo.findAll();
+	}
+	
+	@PostMapping("login")
+	public String loginUser(@RequestBody User user, HttpSession session) {
+		JSONObject json = new JSONObject();
+		if(session.getAttribute("username") == null) {
+			session.setAttribute("username", user.getUsername());
+			json.put("message", "logged in");
+		} else {
+			json.put("message", "already logged in");
+		}
+		return json.toString();	
+		
 	}
 }
