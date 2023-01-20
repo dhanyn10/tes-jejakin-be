f08282f349570f2677b9bd56b47fd419d4522537
dhanyn10 <dhanyn.dhan@gmail.com>
2023-01-20 23:37:20 +0700

    testing

--- order/src/test/java/jejakin/order/OrderApplicationTests.java
+++ order/src/test/java/jejakin/order/OrderApplicationTests.java
@@ -1,5 +1,14 @@
 package jejakin.order;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.IOException;
+
+import org.apache.hc.client5.http.classic.methods.HttpGet;
+import org.apache.hc.client5.http.classic.methods.HttpUriRequest;
+import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
+import org.apache.hc.core5.http.HttpResponse;
+import org.apache.hc.core5.http.HttpStatus;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -7,7 +16,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 class OrderApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads() throws IOException {
+		HttpUriRequest request = new HttpGet("http://localhost:8080/users/all");
+		HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
+		assertEquals(httpResponse.getCode(), HttpStatus.SC_OK);
 	}
-
 }
