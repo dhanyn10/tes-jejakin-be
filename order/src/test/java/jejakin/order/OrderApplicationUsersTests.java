2998c571220928e65725feee13f9f97e72610823
dhanyn10 <dhanyn.dhan@gmail.com>
2023-01-22 01:16:31 +0700

    users test

--- order/src/test/java/jejakin/order/OrderApplicationUsersTests.java
+++ order/src/test/java/jejakin/order/OrderApplicationUsersTests.java
@@ -1,27 +1,78 @@
 package jejakin.order;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
 import java.io.IOException;
-
-import org.apache.hc.client5.http.classic.methods.HttpGet;
-import org.apache.hc.client5.http.classic.methods.HttpUriRequest;
-import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
-import org.apache.hc.core5.http.HttpResponse;
-import org.apache.hc.core5.http.HttpStatus;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import org.apache.commons.io.IOUtils;
+import org.hamcrest.CoreMatchers;
+import org.hamcrest.MatcherAssert;
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+import jejakin.order.dao.UserRepository;
+import jejakin.order.model.User;
+import net.datafaker.Faker;
 
 @SpringBootTest
 @DisplayName("users testing")
 class OrderApplicationUsersTests {
+	
+	@Autowired
+	private UserRepository userRepo;
+	
+	@Value("${webhost}") // ambil data webhost dari application.properties
+	private String alamatHost;
+	
+	@Test
+	@DisplayName("tambah admin")
+	void isertAdmin() throws IOException {
+		userRepo.deleteAll();
+		URL url = new URL("http://"+ alamatHost +":8080/users/admin");
+		URLConnection conn = url.openConnection();
+		InputStream in = conn.getInputStream();
+		String encoding = conn.getContentEncoding();
+		encoding = encoding == null ? "UTF-8" : encoding;
+		String body = IOUtils.toString(in, encoding);
+		MatcherAssert.assertThat(body, CoreMatchers.containsString("admin generated"));
+	}
+	
+	@Test
+	@DisplayName("tambah admin gagal")
+	void isertAdminTwice() throws IOException {
+		URL url = new URL("http://"+ alamatHost +":8080/users/admin");
+		URLConnection conn = url.openConnection();
+		InputStream in = conn.getInputStream();
+		String encoding = conn.getContentEncoding();
+		encoding = encoding == null ? "UTF-8" : encoding;
+		String body = IOUtils.toString(in, encoding);
+		MatcherAssert.assertThat(body, CoreMatchers.containsString("admin only generated once"));
+	}
 
 	@Test
-	@DisplayName("get all user, HttpStatus => OK")
-	void contextLoads() throws IOException {
-		HttpUriRequest request = new HttpGet("http://web:8080/users/all");
-		HttpResponse httpResponse = HttpClientBuilder.create().build().execute(request);
-		assertEquals(httpResponse.getCode(), HttpStatus.SC_OK);
+	@DisplayName("tambah user biasa")
+	void AddRegularkUsers() throws IOException, JSONException {
+		User reguler = new User();
+		Faker faker = new Faker();
+		reguler.setUsername(faker.football().teams());
+		reguler.setFirstname(faker.football().players());
+		reguler.setLastname(faker.football().positions());
+		reguler.setRole("user");
+		reguler.setEmail(faker.football().coaches());
+		userRepo.save(reguler);
+		URL url = new URL("http://"+ alamatHost +":8080/users/all");
+		URLConnection conn = url.openConnection();
+		InputStream in = conn.getInputStream();
+		String encoding = conn.getContentEncoding();
+		encoding = encoding == null ? "UTF-8" : encoding;
+		String body = IOUtils.toString(in, encoding);
+		JSONArray resBody = new JSONArray(body);
+		assertEquals(resBody.length(), 2); // ada 2 user, admin dan reguler
 	}
+	
 }
